7ffac8d0f60c1dd528743072f9ff2e7265013a6a
tonydjy <tonydjy@163.com>
2017-02-26 13:17:30 +0800

    add route for further use

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 var tvapp = angular.module("tvapp", [
-					'tvapp.service'
-]);
+					'tvapp.service',   
+					'ngRoute',
+					'ngAnimate',
+]).config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/',{
+			templateUrl: '/views/admin.html',
+			controller:'MyController'
+		})
+		.when('/test',{
+			templateUrl: '/views/test.html',
+			controller:'MyController'
+		})
+
+	
+}]);
